e7e5640bdc963d5e8c60d48676820a7a409e8d4c
lincode7 <73640055+lincode7@users.noreply.github.com>
2021-03-12 09:15:49 +0800

    Add files via upload

--- packing/packing.py
+++ packing/packing.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-import os, sys
+import os, sys, traceback
 
-# from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-# from PyQt5.QtGui import QIcon, QFontDatabase
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtGui import QIcon, QFontDatabase
+from PyQt5.QtCore import QObject, pyqtSignal
 from UI_packing import Ui_MainWindow
 
 
@@ -27,46 +25,103 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.signal_action()
         self.setui()
 
+    def setui(self):
+        self.ms.visable.emit(self.nameEdit, False)
+        self.ms.visable.emit(self.moreselection, False)
+        self.path = ''
         self.py = []
-        self.ico = r''
+        self.ico = ''
         self.resrc = []
-        self.outpath = r''
+        self.outpath = ''
+        self.ms.visable.emit(self.resources, False)
+        self.ms.visable.emit(self.label_4, False)
+        self.ms.visable.emit(self.line_3, False)
 
-    def packing(self):
-        self.packbutton.setEnabled(False)
+    def signal_action(self):
+        self.python.textChanged.connect(self.setpy)
+        self.icon.textChanged.connect(self.setico)
+        self.resources.textChanged.connect(self.setsource)
+        self.morefilebutton.clicked.connect(self.morefile)
+        self.pathbutton.clicked.connect(self.buildpath)
+        self.clearbutton.clicked.connect(self.clear)
+        self.packbutton.clicked.connect(self.packing)
+        self.checkBox_n.clicked.connect(self.check_n)
+        self.ms.visable.connect(self.setshow)
 
-        # runtimehook = os.path.join(os.getcwd(), 'Filter.ui')
-        # pyinstaller -F -w temp.py -n name -i 1.ico --add-data "test.json;data" --add-data "python.README;data" --clean
-        if self.py:
-            pack = 'pyinstaller'
-            if self.checkBox_F.isChecked():  # 是否生成单文件
-                pack += ' -F'
-            if self.checkBox_w.isChecked():  # 是否关闭控制台
-                pack += ' -w'
+    def setpy(self):
+        # 获取py文件路径，可以多个
+        path = self.python.toPlainText()
 
-            pack += ' ' + self.python.toPlainText()
+        if 'file:///' in path:  # 删除拖放文件的前缀
+            path = path.replace('file:///', '')
+            self.python.setText(path + '\n')
 
-            if self.checkBox_n.isChecked():  # 是否重命名
-                if self.nameEdit.text():
-                    pack += ' -n ' + self.nameEdit.text()
-            if self.checkBox_i.isChecked():  # 是否添加图标
-                if self.icon.toPlainText():
-                    pack += ' -i ' + self.icon.toPlainText()
-                else:
-                    print('no icon')
-                    msg_box = QMessageBox(QMessageBox.Warning, 'Warning', 'no icon!')
-                    msg_box.exec_()
-            if self.resources.toPlainText():  # 是否打包资源文件
-                pack += r' –add-data "' + self.resources.toPlainText() + r':' + 'resources' + r'"'
+            if '\n' in path:  # 多个文件时删除换行符
+                path = path.split('\n')
+            if self.py:  # 第2个及更多py
+                for i in path:
+                    if i not in self.py and i != '':
+                        self.py.append(i)
+            else:  # 第1个py
+                if isinstance(path, str):
+                    self.py.append(path)
+                    if self.outpath == '':  # 从第一个py文件确定默认打包路径
+                        p = os.path.abspath(os.path.dirname(path) + os.path.sep + ".")  # 得到父目录
+                        self.outpath = os.path.join(p, 'build')  # 设置build目录
+                        self.pathEdit.setText(self.outpath)
+                elif isinstance(path, list):
+                    self.py = path[:-1]
+                    if self.outpath == '':  # 从第一个py文件确定默认打包路径
+                        p = os.path.abspath(os.path.dirname(path[0]) + os.path.sep + ".")  # 得到父目录
+                        self.outpath = os.path.join(p, 'build')  # 设置build目录,第一个py文件同目录下简历build文件夹存放生成文件
+                        self.pathEdit.setText(self.outpath)
+            # print('.py:', self.py)
 
-            pack += ' --clean'  # 在每次编译生成exe时，清除之前的编译文件
-            print(pack)
-            # os.system(pack)
+    def setico(self):
+        path = self.icon.toPlainText()
+        if 'file:///' in path:
+            if self.ico:
+                self.ico = (path.replace('file:///', '')).replace(self.ico, '')  # 用新的ico替换原来的ico
+            else:
+                self.ico = path.replace('file:///', '')
+            if self.ico.endswith('.ico'):
+                self.icon.setText(self.ico)
+                # print('.ico:', self.ico)
+            else:
+                self.icon.clear()
+                QMessageBox.warning(self, '警告', '非ico图标文件')
+
+    def setsource(self):
+        # 获取py文件路径，可以多个
+        path = self.resources.toPlainText()
+        if 'file:///' in path:  # 删除拖放文件的前缀
+            path = path.replace('file:///', '')
+            self.resources.setText(path + '\n')
+
+            if '\n' in path:  # 多个文件时删除换行符
+                path = path.split('\n')
+            if self.resrc:  # 第2个及更多资源文件
+                for i in path:
+                    if i not in self.resrc and i != '':
+                        self.resrc.append(i)
+            else:  # 第1个资源文件
+                self.resrc = []
+                if isinstance(path, str):
+                    self.resrc.append(path)
+                elif isinstance(path, list):
+                    self.resrc = path[:-1]
+            # 资源文件分类，img，font，data，ui
+            # print('resrc:', self.resrc)
+
+    def morefile(self):
+        self.morefilebutton.setEnabled(False)
+        if self.moreselection.isVisible():
+            self.ms.visable.emit(self.moreselection, False)
+            self.morefilebutton.setText('more')
         else:
-            print('no python filie!')
-            msg_box = QMessageBox(QMessageBox.Warning, 'Warning', 'no python filie!')
-            msg_box.exec_()
-        self.buttonpack.setEnabled(True)
+            self.ms.visable.emit(self.moreselection, True)
+            self.morefilebutton.setText('↑')
+        self.morefilebutton.setEnabled(True)
 
     def buildpath(self):
         self.pathbutton.setEnabled(False)
@@ -77,77 +132,65 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             QFileDialog.getExistingDirectory(self, "请选择文件夹路径", ".")
         self.pathbutton.setEnabled(True)
 
-    def setui(self):
-        self.ms.visable.emit(self.nameEdit, False)
-
-    def signal_action(self):
-        # print(1)
-        self.python.textChanged.connect(self.setpy)
-        self.icon.textChanged.connect(self.setico)
-        self.resources.textChanged.connect(self.setsource)
-        self.pathbutton.clicked.connect(self.buildpath)
-        self.clearbutton.clicked.connect(self.clear)
-        self.packbutton.clicked.connect(self.packing)
-        self.checkBox_n.clicked.connect(self.check_n)
-        self.ms.visable.connect(self.setshow)
-
-    def setpy(self):
-        # 获取py文件路径，可以多个
-        path = self.python.toPlainText()
-        if 'file:///' in path: # 删除拖放文件的前缀
-            path = path.lstrip('file:///')
-            self.python.setText(path + '\r\n')
-            if self.py == []:  # 第1个py文件入列
-                self.py.append(path)
-            elif '\r\n' in path:  # 第2个开始的py文件加入列表
-                path = path.split('\r\n')
-                for i in path:
-                    if i not in self.py:
-                        self.py.append(i)
-            print(self.py)
-            print(0)
-
-        if self.outpath == '': # 从第一个py文件确定默认打包路径
-            p, f = os.path.split(self.py)
-            self.outpath = os.path.join(p, 'build')
-            self.pathEdit.setText(self.outpath)
-
-
-    def setico(self):
-        path = self.icon.toPlainText()
-        if 'file:///' in path:
-            self.ico = path.lstrip('file:///')
-            self.icon.setText(self.ico)
-
-
-
-    def setsource(self):
-        self.resrc = self.resources.toPlainText()
-        if 'file:///' in self.resrc:
-            self.resrc = self.resrc.lstrip('file:///')
-            self.resources.setText(self.resrc + '\r\n')
-
-
     def clear(self):
+        self.path = ''
         self.py = []
-        self.ico = r''
+        self.ico = ''
         self.resrc = []
-        self.outpath = r''
+        self.outpath = ''
+
         self.python.clear()
         self.icon.clear()
         self.resources.clear()
         self.pathEdit.clear()
         self.nameEdit.clear()
         self.ms.visable.emit(self.nameEdit, False)
+        self.checkBox_F.setChecked(False)
         self.checkBox_n.setChecked(False)
 
+    def packing(self):
+        self.packbutton.setEnabled(False)
+
+        # runtimehook = os.path.join(os.getcwd(), 'Filter.ui')
+        # pyinstaller -p E:\program\build -F -w -n name 1.py 2.py 3.py -i 1.ico --add-data "1.json;data" --add-data "2.font" --add-data "3.png" --clean
+        if self.py:
+            pack = 'pyinstaller'
+            if self.checkBox_F.isChecked():  # 是否生成单文件
+                pack += ' -F'
+            if self.checkBox_w.isChecked():  # 是否关闭控制台
+                pack += ' -w'
+            if self.checkBox_n.isChecked() and self.nameEdit.text():  # 是否重命名
+                pack += r' -n {}'.format(self.nameEdit.text())
+            for i in self.py:
+                if i != '':
+                    pack += r' {}'.format(i)
+            if self.ico:  # 是否添加图标
+                pack += r' -i {}'.format(self.ico)
+            # if self.resrc:  # 是否打包资源文件
+            #     for i in self.resrc:
+            #         if i.endswith('.ttf'):
+            #             pack += r' -–add-data "{};resources\font"'.format(i)
+            #         elif i.endswith('.jpg') or i.endswith('.png') or i.endswith('.ico'):
+            #             pack += r' -–add-data "{};resources\img"'.format(i)
+            #         elif i.endswith('.ui'):
+            #             pack += r' -–add-data "{};resources\ui"'.format(i)
+            #         else:
+            #             pack += r' -–add-data "{};resources\data"'.format(i)
+
+            pack += r' --workpath {}  --distpath {} --specpath {} -y --clean'.format(os.path.join(self.outpath, 'temp'), os.path.join(self.outpath, 'final'),self.outpath)  # 在每次编译生成exe时，清除之前的编译文件
+            print(pack)
+            os.system(pack)
+        else:
+            print('no python filie!')
+            QMessageBox.warning(self, 'Warning', 'no python filie!')
+
+        self.packbutton.setEnabled(True)
 
     def check_n(self):
         if self.checkBox_n.isChecked():
             self.ms.visable.emit(self.nameEdit, True)
         else:
             self.ms.visable.emit(self.nameEdit, False)
-
 
     def setshow(self, ui, flag):
         if isinstance(ui, list):
@@ -158,7 +201,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MW = MainWindow()
-    MW.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        MW = MainWindow()
+        MW.show()
+        sys.exit(app.exec_())
+    except Exception:
+        print(traceback.format_exc())
